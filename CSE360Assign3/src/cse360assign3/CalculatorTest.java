fe78ce7d8b9c1efee69c8d083d51eb6281852078
Zackary Crosley <Zackary Crosley@LAPTOP-LEO9OR1H>
2016-02-19 20:27:03 -0700

    Commit Step 2 - Added JUnit test cases

---
+++ CSE360Assign3/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,44 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testCalculator() {
+		Calculator test = new Calculator();
+		assertNotNull(test);
+	}
+	
+	public void testGetTotal () {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAdd (int value) {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSubtract (int value) {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testMultiply (int value) {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDivide (int value) {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetHistory () {
+		fail("Not yet implemented");
+	}
+
+}
