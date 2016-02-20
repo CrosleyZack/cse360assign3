465aefe986e218c3e975e92a5814c3a907f32f49
Zackary Crosley <Zackary Crosley@LAPTOP-LEO9OR1H>
2016-02-19 21:09:34 -0700

    Second Step Commit - with JUnit tests added

--- CSE360Assign3/src/cse360assign3/Calculator.java
+++ CSE360Assign3/src/cse360assign3/Calculator.java
@@ -4,30 +4,66 @@ public class Calculator {
 
 	private int total;
 	
+	/**
+	 * Constructor for calculator class. Initializes total to 0
+	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	 /**
+	  * Returns the value stored in total.
+	  * 
+	  * @return total - returns the value stored in int total, or the value after the user has run any number
+	  * 	of operations
+	  */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
+	/**
+	 * Adds parameter value to total and stores the sum in total
+	 * 
+	 * @param value - value to add into total
+	 */
 	public void add (int value) {
-		
+		total += value; 
 	}
 	
+	/**
+	 * Subtracts value from total and stores difference in total
+	 * 
+	 * @param value - value to subtract from total
+	 */
 	public void subtract (int value) {
-		
+		total -= value;
 	}
 	
+	/**
+	 * Multiplies value to total and stores product in total
+	 * 
+	 * @param value - value to multiply the total by
+	 */
 	public void multiply (int value) {
-		
+		total *= value;
 	}
 	
+	/**
+	 * Divides total by value and stores quotient in total
+	 * 
+	 * @param value - value to divide the total by
+	 */
 	public void divide (int value) {
-		
+		if (value == 0)
+			return;
+		total /= value;
 	}
 	
+	/**
+	 * 
+	 * 
+	 * @return
+	 */
 	public String getHistory () {
 		return "";
 	}
--- CSE360Assign3/src/cse360assign3/CalculatorTest.java
+++ CSE360Assign3/src/cse360assign3/CalculatorTest.java
@@ -6,39 +6,153 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
+	/**
+	 * Tests constructor
+	 * 
+	 */
 	@Test
 	public void testCalculator() {
 		Calculator test = new Calculator();
 		assertNotNull(test);
 	}
 	
+	
+	/**
+	 * Tests getTotal as well as basic addition functionality
+	 * 
+	 */
+	@Test
 	public void testGetTotal () {
-		fail("Not yet implemented");
+		Calculator test = new Calculator();
+		int total = test.getTotal();
+		assertEquals(total, 0);
+		
+		test.add(3);
+		total = test.getTotal();
+		assertEquals(total,3);
 	}
 	
+	
+	/**
+	 * tests add method functionality by adding 0, positive, and negative numbers.
+	 * 
+	 */
 	@Test
-	public void testAdd (int value) {
-		fail("Not yet implemented");
-	}
+	public void testAdd () {
+		Calculator test = new Calculator();
+		int total = test.getTotal();
+		assertEquals(total, 0);
+		
+		test.add(4);
+		total = test.getTotal();
+		assertEquals(total,4);
+		
+		test.add(0);
+		total = test.getTotal();
+		assertEquals(total, 4);
+		
+		test.add(-6);
+		total = test.getTotal();
+		assertEquals(total,-2);
+		}
 	
+	
+	/**
+	 * tests subtract method functionality by subtracting 0, positive, and negative numbers.
+	 * 
+	 */
 	@Test
-	public void testSubtract (int value) {
-		fail("Not yet implemented");
+	public void testSubtract () {
+		Calculator test = new Calculator();
+		int total = test.getTotal();
+		assertEquals(total, 0);
+		
+		test.subtract(4);
+		total = test.getTotal();
+		assertEquals(total,-4);
+		
+		test.subtract(0);
+		total = test.getTotal();
+		assertEquals(total, -4);
+		
+		test.subtract(-5);
+		total = test.getTotal();
+		assertEquals(total,1);
 	}
 	
+	
+	/**
+	 * tests multiply method functionality by multiplying by positive values, negative values, and zero.
+	 * 
+	 */
 	@Test
-	public void testMultiply (int value) {
-		fail("Not yet implemented");
+	public void testMultiply () {
+		Calculator test = new Calculator();
+		int total = test.getTotal();
+		assertEquals(total, 0);
+		
+		test.multiply(4);
+		total = test.getTotal();
+		assertEquals(total,0);
+		
+		//for multiply to reach non-zero number
+		test.add(1);
+		total = test.getTotal();
+		assertEquals(total, 1);
+		
+		test.multiply(4);
+		total = test.getTotal();
+		assertEquals(total,4);
+		
+		test.multiply(-3);
+		total = test.getTotal();
+		assertEquals(total,-12);
+		
+		test.multiply(0);
+		total = test.getTotal();
+		assertEquals(total,0);
 	}
 	
+	
+	/**
+	 * tests divide method functionality by dividing by positive values, negative values, and zero.
+	 * 
+	 */
 	@Test
-	public void testDivide (int value) {
-		fail("Not yet implemented");
+	public void testDivide () {
+		Calculator test = new Calculator();
+		int total = test.getTotal();
+		assertEquals(total, 0);
+		
+		test.divide(4);
+		total = test.getTotal();
+		assertEquals(total,0);
+		
+		//for divide to reach non-zero number
+		test.add(24);
+		total = test.getTotal();
+		assertEquals(total, 24);
+		
+		test.divide(-4);
+		total = test.getTotal();
+		assertEquals(total,-6);
+		
+		test.divide(3);
+		total = test.getTotal();
+		assertEquals(total,-2);
+		
+		test.divide(0);
+		total = test.getTotal();
+		assertEquals(total,-2);
 	}
 	
+	
+	/**
+	 * Tests history functionality by printing and verifying strings.
+	 * 
+	 */
 	@Test
 	public void testGetHistory () {
 		fail("Not yet implemented");
 	}
-
 }
